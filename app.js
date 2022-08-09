e6387e6cfeab8e5ef8dab22c00845640125bf39f
Cory Nelson <fullnelsontech@gmail.com>
2022-08-08 21:48:33 -0400

    Helper functions broke app, fix later

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
 const exphbs = require("express-handlebars")
+
+const methodOverride = require("method-override")
 const passport = require("passport")
 const session = require("express-session")
 const MongoStore = require("connect-mongo")
@@ -18,10 +20,35 @@ connectDB()
 
 const app = express()
 
+//BODY PARSER
+// app.use(express.urlencoded({ extended: false }))
+// app.use(express.json())
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+)
+
 //Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
 }
+
+//HELPERS
+// const {
+//   formatDate,
+//   stripTags,
+//   truncate,
+//   editIcon,
+//   select,
+// } = require("./helpers/hbs")
 
 // HandleBars
 // add .engine after exphbs
