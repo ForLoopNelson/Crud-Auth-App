eda82eca055866a6b1a97d2d19f11906250077ad
Cory Nelson <fullnelsontech@gmail.com>
2022-08-08 20:44:36 -0400

    still in progress. not serving up add.hbs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require("morgan")
 const exphbs = require("express-handlebars")
 const passport = require("passport")
 const session = require("express-session")
+const MongoStore = require("connect-mongo")
 const connectDB = require("./config/db")
 
 // Load config
@@ -33,6 +34,9 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+    }),
   })
 )
 
@@ -46,6 +50,7 @@ app.use(express.static(path.join(__dirname, "public")))
 // Routes
 app.use("/", require("./routes/index"))
 app.use("/auth", require("./routes/auth"))
+app.use("/stories", require("./routes/stories"))
 
 const PORT = process.env.PORT || 8500
 
